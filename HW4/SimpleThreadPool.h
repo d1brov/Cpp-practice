05440eae52fec054957dcd4e0e82656f030cd168
Andrii Dibrov <andrii.dibrov@gmail.com>
2022-12-20 08:20:40 +0200

    Added HW4 task

---
+++ HW4/SimpleThreadPool.h
@@ -0,0 +1,26 @@
+#pragma once
+
+#include <vector>
+#include <queue>
+#include <future>
+#include <thread>
+
+
+class SimpleThreadPool {
+public:
+	explicit SimpleThreadPool(std::size_t threadCount);
+	~SimpleThreadPool();
+	SimpleThreadPool(const SimpleThreadPool&) = delete;
+	SimpleThreadPool& operator = (const SimpleThreadPool&) = delete;
+	template < typename Fnc_T >
+	auto Post(Fnc_T task) -> std::future < decltype(task()) >;
+	void WorkOn();
+	void Destroy();
+private:
+	size_t m_threadCount;
+	std::vector < std::thread > threads;
+	std::queue < std::function < void() >> tasks;
+	std::condition_variable condition;
+	std::mutex mut;
+	bool stop;
+};
